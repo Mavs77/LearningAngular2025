f67d615b0f7bd04eaddc294d76675c165a4d8362
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2025-03-04 19:41:24 -0600

    demo app downloaded

--- ch01/my-app/src/main.ts
+++ ch01/my-app/src/main.ts
@@ -2,5 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
+
+// The startup method of an Angular application is called bootstrapping, and it is defined in the main.ts file inside the src folder
+
+// The main tas of the bootstrapping file is to define the component that will be loaded at application startup.
